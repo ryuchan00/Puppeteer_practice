e0409e87c8fbb6a07c7f2afe7bdbafbcd9e182e1
ryuchun00 <leokun0210@gmail.com>
2018-12-12 00:35:07 +0900

    自動tweetする

---
+++ Puppeteer/login_password2.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+
+require('dotenv').config();
+
+const USER_ID = process.env.MY_USER_ID;
+const PASSWORD = process.env.MY_PASSWORD;
+
+function nowtime(){
+    var d = new Date();
+    var year = d.getYear()+1900;
+    var month = d.getMonth()+1;
+    var date = d.getDate();
+    var hour = d.getHours();
+    var minute = d.getMinutes();
+    var second = d.getSeconds();
+    d = year + "年" + month + "月" + date + "日" + hour + "時" + minute + "分"+ second + "秒でーす";
+    return d;
+}
+
+//メインロジック
+(async() => {
+// Puppeteerの起動
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 50,
+    });
+
+    // 新しいからのページを開く
+    const page = await browser.newPage();
+
+    // view portの設定
+    await page.setViewport({
+        width: 1200,
+        height: 800,
+    });
+
+    await page.goto('https://twitter.com/login');
+
+    // ユーザーIDを入力する
+    await page.type('input.js-username-field', USER_ID);
+    // パスワードを入力する
+    await page.type('input.js-password-field', PASSWORD);
+
+    // ログインボタンをクリックする
+    await page.click('button[type="submit"]');
+
+    await page.goto('https://twitter.com/');
+    await page.waitFor(3000);
+    await page.click('button#global-new-tweet-button');
+    await page.waitFor(3000);
+    await page.keyboard.type(nowtime());
+    await page.waitFor(3000);
+    await page.click('button.js-send-tweets');
+
+    // ブラウザの終了
+    await browser.close();
+})();
